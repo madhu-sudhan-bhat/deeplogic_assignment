2f80d63333c5b9156acc6c807516352c141ec520
madhu sudhan bhat <55833862+madhu-sudhan-bhat@users.noreply.github.com>
2020-08-14 10:54:26 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,33 @@
+const https = require('https');
+
+let url = "https://time.com/";
+var finalanswer
+https.get(url,(res) => {
+    let body = "";
+
+    res.on("data", (chunk) => {
+        body += chunk;
+    });
+
+    res.on("end", () => {
+        
+            let jsondata = JSON.stringify(body);
+            const htmlString = jsondata
+//const result = htmlString.match(/<section class="homepage-module latest" data-module_name="Latest Stories">/g);
+           // var answer =body.match(/<section.*class="homepage-module latest. +.*[\n]+.*/igm)
+           var answer =body.match(/<section.*class="homepage-module latest" data-module_name="Latest Stories".*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]+.*[\n]/igm);
+          var res = answer.toString();
+           finalanswer = res.match(/<h2.*class="title.*[\n]/igm)
+           console.log(finalanswer);
+    });
+
+})
+
+const express = require('express'),
+    app = express()
+
+    
+
+app.post('/getnews', (req, res) => res.json(finalanswer));
+
+app.listen(3000, () => console.log('Server start at port 3000'))
